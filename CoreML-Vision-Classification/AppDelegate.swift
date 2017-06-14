d33868de8fdad8431b14f47e543479c1a265bc1d
Adam McDonald <adam@l4digital.com>
2017-06-13 19:12:26 -0700

    Initial sample app.

---
+++ CoreML-Vision-Classification/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CoreML-Vision-Classification
+//
+//  Created by Adam McDonald on 6/13/17.
+//  Copyright © 2017 L4 Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
